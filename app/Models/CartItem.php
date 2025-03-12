2c473b5b1dd12a7016f450765cfadec56ccfcef2
ziadadel001 <ziadadel00120@gmail.com>
2025-03-12 21:55:48 +0200

    create fillable fot models

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'cart_id',
+        'product_id',
+        'quantity',
+        'price',
+    ];
     public function cart()
     {
         return $this->belongsTo(Cart::class);
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+    ];
     public function cartItems()
     {
         return $this->hasMany(CartItem::class);
